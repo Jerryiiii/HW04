a4b70cb51e6879f0283e03f9134e5271644d0d90
Jerryiiii <31806359+Jerryiiii@users.noreply.github.com>
2017-10-01 16:46:30 -0700

    Create sketch-7.js

---
+++ sketch-7.js
@@ -0,0 +1,15 @@
+background(255)
+noFill()
+var x = 2
+var y = 2
+var z = 60
+while (true){
+var x = 2
+var y = 2 
+while(x<= 52){
+    rect((z-x)/2,(height-y)/2, x, y)
+  x = x + 10
+  y = y + 10
+}
+  z= z + 140
+}
